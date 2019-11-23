40bc0858fddba20b818e5a9acf6df4e48ef0ad6d
nvquoctuan <nvquoctuan@gmail.com>
2019-11-23 13:48:38 +0700

    Subpitch show list and one for user

--- app/models/pitch.rb
+++ app/models/pitch.rb
@@ -2,5 +2,31 @@ class Pitch < ApplicationRecord
   belongs_to :user
   has_many :subpitches, dependent: :destroy
 
+  NUMBER_FORMAT = /\A[\d]+\z/i.freeze
+  PARAMS = %i(name description country city district address picture start_time
+            end_time limit).freeze
+  belongs_to :user
+
+  validates :name, presence: true, length: {maximum: Settings.size.s50}
+  validates :description, length: {maximum: Settings.size.s255}
+  validates :country, :city, :district,
+            presence: true, length: {maximum: Settings.size.s50}
+  validates :address, presence: true, length: {maximum: Settings.size.s100}
+  validates :phone, presence: true, format: {with: NUMBER_FORMAT},
+            length: {in: Settings.size.s6..Settings.size.s10}
+  validates :end_time, date: {after: :start_time}
+  validates :limit, presence: true, format: {with: NUMBER_FORMAT},
+            length: {in: Settings.size.s1..Settings.size.s2}
+
+  has_one_attached :picture
+
+  scope :search, (lambda do |pitch_name|
+    if pitch_name
+      where("name LIKE ?", "%#{pitch_name}%")
+    else
+      all
+    end
+  end)
+
   scope :latest_pitches, ->{order(created_at: :desc)}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,17 +12,18 @@ Rails.application.routes.draw do
     resources :users
     resources :bookings
   end
+
   post "/login", to: "sessions#create"
   get "/signup", to: "users#new"
   get "/login", to: "sessions#new"
   get "/logout", to: "sessions#destroy"
   match "/auth/:provider/callback", to: "sessions#create", via: [:get, :post]
   match "/auth/failure", to: "sessions#failure", via: [:get, :post]
+
   get "/blog", to: "static_pages#blog"
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
   resources :password_resets, except: :index
-  resources :users
   resources :account_activations, only: :edit
   resources :pitches, only: :index do
     resources :subpitches, only: %i(index show)
@@ -38,4 +39,5 @@ Rails.application.routes.draw do
   patch "pays/update"
   patch "bookings/update", to: "bookings#update"
   post "bookings/create"
+  resources :users
 end
