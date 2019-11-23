1d3b0b6f07d7ff2a18434add8a433e09aed4a447
nvquoctuan <nvquoctuan@gmail.com>
2019-11-23 13:48:38 +0700

    Subpitch show list and one for user

--- app/models/subpitch.rb
+++ app/models/subpitch.rb
@@ -2,6 +2,7 @@ class Subpitch < ApplicationRecord
   belongs_to :pitch
   belongs_to :subpitch_type
   has_many :bookings, dependent: :destroy
+
   has_one_attached :picture
   scope :pitch, ->(pitch_id){where("pitch_id = \"?\"", pitch_id)}
 end
