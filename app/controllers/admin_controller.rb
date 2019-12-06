nvquoctuan/dn_sfms_29

Check admin, owner

app/controllers/admin_controller.rb
@@ -1,3 +1,22 @@
 class AdminController < ApplicationController
+  before_action :logged_in?
+  before_action :check_user
+
   layout "admin/application"
+
+  private
+
+  def check_admin
+    return if current_user.admin?
+
+    redirect_to root_path
+    flash[:danger] = t "admin.danger_permission"
+  end
+
+  def check_user
+    return unless current_user.user?
+
+    redirect_to root_path
+    flash[:danger] = t "admin.danger_permission"
+  end
 end

nvquoctuan/demo_soccer_fields

app/controllers/admin_controller.rb
@@ -53,4 +53,11 @@ class AdminController < ApplicationController
     flash[:danger] = t "msg.danger_permission"
     redirect_to admin_pitches_path
   end
+
+  def check_user
+    return unless current_user.user?
+
+    redirect_to root_path
+    flash[:danger] = t "msg.danger_permission"
+  end
 end