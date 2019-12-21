26ddfe71767f1758a1b6321ff0612c49885ccb8c
nvquoctuan <nvquoctuan@gmail.com>
2019-12-21 21:47:52 +0700

    Rspec_full_user with google_map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,9 @@
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
 //= require bootstrap
 //= require custom
 //= require moment
 //= require bootstrap-datetimepicker
 //= require social-share-button
+//= require gmaps/google
+//= require_tree .
--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -72,5 +72,3 @@ $(function () {
     format: 'YYYY-MM-DD'
   });
 });
-
-
